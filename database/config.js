5bb73a8ea653a14472bf8d100da00e1ec068ff9f
dyyao <646617513@qq.com>
2018-03-20 16:11:33 +0800

    修改了数据库的链接

--- database/config.js
+++ database/config.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/boss');
+mongoose.connect('mongodb://localhost/yaoyao');
 db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
